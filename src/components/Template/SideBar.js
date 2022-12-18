227f9ee1e167769f360f5c0859a2a3c1ba85490f
gihanmora <gihangamage.15@cse.mrt.ac.lk>
2022-12-19 02:58:00 +1100

    Changed image sizes

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" style={{ height: '100px', width: '100px' }} />
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" style={{ height: '300px', width: '300px' }} />
       </Link>
       <header>
         <h2>Gihan Gamage</h2>
